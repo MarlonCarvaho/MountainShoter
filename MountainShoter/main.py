4fb7d895aa8ae41fcaa67f9f3154d9748f877fcb
Marlon Oliveira <marloncibh00@gmail.com>
2026-04-26 12:48:35 -0300

    comit inicial

---
+++ MountainShoter/main.py
@@ -0,0 +1,16 @@
+import sys
+
+import pygame
+
+print("Que comece o jogo")
+pygame.init()
+window = pygame.display.set_mode(size=(800, 600))
+print("Que comece o jogo")
+
+print("começa a carregar")
+while True:
+    # chegando todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # esse fecha a janela
+            sys.exit() # esse encerra a janela
