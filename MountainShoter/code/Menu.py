b278aa59cded8cc9481bdb5b8c09e9c2ff7746e1
Marlon Oliveira <marloncibh00@gmail.com>
2026-05-03 11:22:13 -0300

    atualizando tela menu

--- MountainShoter/code/Menu.py
+++ MountainShoter/code/Menu.py
@@ -20,6 +20,12 @@ class Menu:
         text_rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
 
+
+    def normal_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        font = pygame.font.SysFont("Lucida Sans Typewriter", text_size)
+        text_surf = font.render(text, True, text_color).convert_alpha()
+        self.window.blit(source=text_surf, dest=text_pos)
+
     def run(self):
         menu_running = True
         while menu_running:
@@ -41,12 +47,18 @@ class Menu:
                         return menu_option[self.opcao_selecionada]
 
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "jogo de terror", color_vermelho, (400, 200))
+            self.menu_text(50, "jogo de terror", color_vermelho, (400, 150))
 
             for i in range(len(menu_option)):
                 if i == self.opcao_selecionada:
-                    self.menu_text(30, menu_option[i], color_azul, (400, 300 + 30 * i))
+                    self.menu_text(30, menu_option[i], color_azul, (400, 250 + 30 * i))
                 else:
-                    self.menu_text(30, menu_option[i], color_branco, (400, 300 + 30 * i))
+                    self.menu_text(30, menu_option[i], color_branco, (400, 250 + 30 * i))
+
+
+            self.normal_text(15, "CONTROLES:", color_vermelho, (10, 500))
+            self.normal_text(15, "P1: W,A,S,D (Move) | ESPAÇO (Atira)", color_branco, (10, 520))
+            self.normal_text(15, "P2: SETAS (Move)   | ENTER (Atira)", color_branco, (10, 540))
+            # ----------------------------------------------
 
             pygame.display.flip()
